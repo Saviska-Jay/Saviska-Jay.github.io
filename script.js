d50e25ed21ec633c1c2c52dd5367c79625c4c740
Saviska Jayawickrama <158861381+Saviska-Jay@users.noreply.github.com>
2024-06-25 23:24:11 +0530

    Add a JS function to e to calculate °C to °F

---
+++ script.js
@@ -0,0 +1,13 @@
+function Convert() {
+    var Celsius = document.getElementById("C").value;
+    var Farenheit = (9 / 5 * Celsius) + 32;
+    var result = document.getElementById("result");
+
+    if (Celsius == "") {
+        alert("🙄 Please Enter a Number ! 🙄");
+        document.getElementById("C").focus;
+    }
+    else {
+        result.innerHTML =" " + Celsius + " °C = " +  "<mark>" + Farenheit +" &nbsp °F </mark> <br>"
+    }
+}
