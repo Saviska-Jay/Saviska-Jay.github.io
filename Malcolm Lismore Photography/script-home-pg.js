657c8fab906e2bd3d4a35cd5a80f42e9315b0692
Saviska Jayawickrama <158861381+Saviska-Jay@users.noreply.github.com>
2024-06-29 17:27:18 +0530

    Entered the Gallery preview section, its js code, and About Me Preview Section

---
+++ Malcolm Lismore Photography/script-home-pg.js
@@ -0,0 +1,45 @@
+var myIndex1 = 0;
+carousel1();
+
+function carousel1() {
+  var i;
+  var x = document.getElementsByClassName("gallery-preview-slideshow1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex1++;
+  if (myIndex1 > x.length) {myIndex1 = 1}    
+  x[myIndex1-1].style.display = "block";  
+  setTimeout(carousel1, 3000);    
+}
+
+var myIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("gallery-preview-slideshow2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}    
+  x[myIndex2-1].style.display = "block";  
+  setTimeout(carousel2, 3000);    
+}
+
+
+var myIndex3 = 0;
+carousel3();
+
+function carousel3() {
+  var i;
+  var x = document.getElementsByClassName("gallery-preview-slideshow3");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex3++;
+  if (myIndex3 > x.length) {myIndex3 = 1}    
+  x[myIndex3-1].style.display = "block";  
+  setTimeout(carousel3, 3000);    
+}
